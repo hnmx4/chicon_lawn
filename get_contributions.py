5c492c7dac3d6bc1ed66a4c8f0383862761a1c7b
hnmx4 <haneuma0628@gmail.com>
2016-06-16 23:57:25 +0900

    be simple

--- get_contributions.py
+++ get_contributions.py
@@ -2,25 +2,12 @@
 
 import sys
 import json
-
+import urllib2
 from bs4 import BeautifulSoup
 
 
-def pick_count_week_date_list(res):
-  lists = []
-  dict_obj = {}
-  html = res.read()
-  soup = BeautifulSoup(html, 'html.parser')
-  for rect in soup.find_all('rect'):
-    dict_obj = {
-      'data-date'  : rect['data-date'],
-      'data-count' : rect['data-count']
-      }
-    lists.append(dict_obj)
-  return lists
-
-
-def pick_count_list(res):
+def pick_count_list(url):
+  res = urllib2.urlopen(urllib2.Request(url))
   lists = []
   html = res.read()
   soup = BeautifulSoup(html, 'html.parser')
