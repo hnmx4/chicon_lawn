e3989236c1e22328e5007066713114352a9b011d
hnmx4 <haneuma0628@gmail.com>
2016-06-16 22:37:38 +0900

    add getting contributions func

---
+++ get_contributions.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+from bs4 import BeautifulSoup
+
+
+def pick_count_week_date_list(res):
+  lists = []
+  dict_obj = {}
+  html = res.read()
+  soup = BeautifulSoup(html, 'html.parser')
+  for rect in soup.find_all('rect'):
+    dict_obj = {
+      'data-date'  : rect['data-date'],
+      'data-count' : rect['data-count']
+      }
+    lists.append(dict_obj)
+  return lists
+
+
+def pick_count_list(res):
+  lists = []
+  html = res.read()
+  soup = BeautifulSoup(html, 'html.parser')
+  for rect in soup.find_all('rect'):
+    lists.append(rect['data-count'])
+  return lists
