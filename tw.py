6ffd6918c80b3f0bc030c749a497f5736be9d31b
hnmx4 <haneuma0628@gmail.com>
2016-06-17 00:00:27 +0900

    implement function that change icon according to contributions

--- tw.py
+++ tw.py
@@ -4,6 +4,8 @@ import tweepy
 import dotenv
 from os.path import join, dirname
 
+import get_contributions as gc
+
 
 def denv(envkey):
     return dotenv.get_key(join(dirname(__file__), '.env'), envkey)
@@ -11,3 +13,10 @@ def denv(envkey):
 auth = tweepy.OAuthHandler(denv('CONSUMER_KEY'), denv('CONSUMER_SECRET'))
 auth.set_access_token(denv('ACCESS_TOKEN'), denv('ACCESS_SECRET'))
 api = tweepy.API(auth)
+
+url = 'https://github.com/users/' + denv('USER') + '/contributions'
+lists = gc.pick_count_list(url)
+nc = int(lists[-1])
+select = lambda c: '4' if c > 4 else str(c)
+
+api.update_profile_image(select(nc)+'.png')
