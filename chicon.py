c14ce21939fe6eb6e3f9f6898229edceb5d62229
hnmx4 <haneuma0628@gmail.com>
2016-12-22 01:28:06 +0900

    little change

--- chicon.py
+++ chicon.py
@@ -1,12 +1,11 @@
 # -*- coding:utf-8 -*-
-
 import tweepy
 import dotenv
 import datetime
 import calendar
 
 from bs4 import BeautifulSoup
-from os.path import join, dirname, abspath, devnull
+from os import path
 from selenium import webdriver
 
 
@@ -17,7 +16,6 @@ LEVEL_HUB = {
     '#44a340': 3,
     '#1e6823': 4
 }
-
 LEVEL_LAB = {
     '#ededed': 0,
     '#acd5f2': 1,
@@ -28,25 +26,22 @@ LEVEL_LAB = {
 
 
 def denv(envkey):
-    return dotenv.get_key(join(dirname(__file__), '.env'), envkey)
+    return dotenv.get_key(path.join(path.dirname(__file__), '.env'), envkey)
 
 
 def get_soup(pform, username):
-    driver = webdriver.PhantomJS(service_log_path=devnull)
+    driver = webdriver.PhantomJS(service_log_path=path.devnull)
 
     if pform == 'github':
         url = 'https://github.com/users/' + str(username) + '/contributions'
     elif pform == 'gitlab':
         url_login = 'https://gitlab.com/users/sign_in'
         driver.get(url_login)
-
         loginid = driver.find_element_by_xpath('//input[@id="user_login"]')
         passwd = driver.find_element_by_xpath('//input[@id="user_password"]')
         loginid.send_keys(denv('USER_GITLAB'))
         passwd.send_keys(denv('PASSWD_GITLAB'))
-
         driver.find_element_by_name('commit').submit()
-
         url = 'https://gitlab.com/u/' + str(username)
 
     driver.get(url)
@@ -67,35 +62,33 @@ def pick_dayly_level(username_hub, username_lab):
     months = {}
     for i, v in enumerate(calendar.month_abbr):
         months[v] = i
-    for rect in soup_lab.find_all('rect', attrs={'class': 'user-contrib-cell'}):
-        src = rect['data-original-title']
-        # gitlab date format is like "Thursday Jul 21 2016"
-        date = src.split('<br />')[1]
-        date = date.split()
-        # make date format the same like "2016-07-21"
-        date = (
-            str(date[3]) + '-' +
-            '{0:0>2}'.format(str(months[date[1]])) + '-' +
-            '{0:0>2}'.format(str(date[2]).rstrip(','))
-        )
+    for rect in soup_lab.find_all('rect',
+                                  attrs={'class': 'user-contrib-cell'}):
+        src = rect['data-original-title'].split('>')[1].split()
+        date = '-'.join([
+            str(src[3]),
+            '{0:0>2}'.format(str(months[src[1]])),
+            '{0:0>2}'.format(str(src[2]).rstrip(','))
+        ])
         if date in data:
             data[date] += LEVEL_LAB[rect['fill']]
         else:
-            data[rect[date]] = LEVEL_LAB[rect['fill']]
+            data[date] = LEVEL_LAB[rect['fill']]
 
     return data
 
-
-auth = tweepy.OAuthHandler(denv('CONSUMER_KEY'), denv('CONSUMER_SECRET'))
-auth.set_access_token(denv('ACCESS_TOKEN'), denv('ACCESS_SECRET'))
-api = tweepy.API(auth)
 
 lv = pick_dayly_level(denv('USER_GITHUB'), denv('USER_GITLAB'))
 yday = datetime.date.today() - datetime.timedelta(1)
 yday = yday.strftime('%Y-%m-%d')
 
+auth = tweepy.OAuthHandler(denv('CONSUMER_KEY'), denv('CONSUMER_SECRET'))
+auth.set_access_token(denv('ACCESS_TOKEN'), denv('ACCESS_SECRET'))
+api = tweepy.API(auth)
+
 print('-----' + str(datetime.datetime.today()) + '-----')
 api.update_profile_image(
-    abspath(dirname(__file__)) + '/icons/' +
-    (lambda x: 'ex' if x > 4 else str(x))(lv[yday]) + '.png'
+    path.join(path.abspath(path.dirname(__file__)),
+              'icons',
+              (lambda x: 'ex' if x > 4 else str(x))(lv[yday]) + '.png')
 )
